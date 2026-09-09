1879727b81b9415143c626bfdaaef875135d618d
jackmuama <mua.lijun@gmail.com>
2026-09-09 21:55:40 +0800

    msg: 解构化

--- src/main/kotlin/NyetHack.kt
+++ src/main/kotlin/NyetHack.kt
@@ -1,8 +1,20 @@
 package org.example
 
+import java.io.File
+import java.nio.charset.StandardCharsets
+
 fun main() {
     println(profile("Hello World"))
 //    val heroName = readLine() ?: "无名";
+    val file = File("data/menu.txt")
+    val text = file.readText(StandardCharsets.UTF_8)
+    val menus: List<String> = text.split("\n")
+    val list = List(menus.size) { i ->
+        // 解构化
+        val (_, english) = menus[i].split(",")
+        english
+    }
+    list.forEach { println(it) }
 }
 
 fun narrate(msg: String) {
